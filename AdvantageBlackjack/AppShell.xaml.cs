5674bb57df2337bbd23812358b8fec04a8d298ef
Isaiah <igavin13@ksu.edu>
2025-03-01 16:15:52 -0600

    Add project files.

---
+++ AdvantageBlackjack/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AdvantageBlackjack
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AdvantageBlackjack/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AdvantageBlackjack
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
