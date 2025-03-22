da851aa23716c081a2e08e0376686fa8748cb2f1
Isaiah Gavin <igavin13@ksu.edu>
2025-03-21 21:38:32 -0500

    Finished MainPage animation

--- AdvantageBlackjack/Pages/MainPage.xaml.cs
+++ AdvantageBlackjack/Pages/MainPage.xaml.cs
@@ -5,6 +5,35 @@
         public MainPage()
         {
             InitializeComponent();
+            InitializeOpacity();
+            AnimateUI();
+        }
+
+        private async void AnimateUI()
+        {
+            await Task.Delay(1000);  // Wait before animating
+            await LogoImage.FadeTo(1, 1750, Easing.CubicInOut); 
+            await FadeIn(Subheader, 0);
+            await FadeIn(BasicStrategyBtn, 0);
+            await FadeIn(PairsAndSoftHandsBtn, 0);
+            await FadeIn(StrategyTableBtn, 0);
+            await FadeIn(CountingBtn, 0);
+        }
+
+        private void InitializeOpacity()
+        {
+            LogoImage.Opacity = 0;
+            Subheader.Opacity = 0;
+            BasicStrategyBtn.Opacity = 0;
+            PairsAndSoftHandsBtn.Opacity = 0;
+            StrategyTableBtn.Opacity = 0;
+            CountingBtn.Opacity = 0;
+        }
+
+        private async Task FadeIn(View element, int delay)
+        {
+            await Task.Delay(delay);  // Wait before animating
+            await element.FadeTo(1, 100, Easing.CubicInOut);  // Smooth fade-in over 0.8s
         }
 
         /// <summary>
@@ -22,7 +51,7 @@
         /// </summary>
         /// <param name="sender">sender</param>
         /// <param name="e">e</param>
-        private void OptionsClicked(object sender, EventArgs e)
+        private void CountingClicked(object sender, EventArgs e)
         {
             DisplayAlert("Error", "Not Implemented", "Cancel");
         }
